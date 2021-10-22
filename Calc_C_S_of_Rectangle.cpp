8ba29127f5923e67ddc5139ad57bcd834273392c
Tuan2002 <64023863+Tuan2002@users.noreply.github.com>
2021-10-22 08:44:14 +0700

    Update and fix bug

--- Calc_C_S_of_Rectangle.cpp
+++ Calc_C_S_of_Rectangle.cpp
@@ -19,31 +19,35 @@ int main()
         case 1:
         {
             //input data
-            printf("Enter the height of rectangle = ");
+            printf("Enter the height of rectangle: ");
             scanf("%f", &a);
-            printf("Enter the width of rectangle = ");
+            printf("Enter the width of rectangle: ");
             scanf("%f", &b);
             //The Circumference
             c = (a+b)*2;
-            printf("The Circumference of rectangle with height = %f and width = %f is %.2f\n",a ,b ,c);
+            printf("The Circumference of rectangle with height = %.2f and width = %.2f is %.2f\n",a ,b ,c);
             //Area
             s = a*b;
-            printf("The Area of rectangle with height = %f and width = %f is %.2f\n",a ,b ,s);
+            printf("The Area of rectangle with height = %.2f and width = %.2f is %.2f\n",a ,b ,s);
+            printf("====================================\n");
+            printf("Copyright by Tuan Nguyen 62K1 - CNTT - Vinh University ");
             break;
         }
         case 2:
         {
             //Nhap so lieu
-            printf("Xin hay nhap chieu dai cua hinh chu nhat ");
+            printf("Xin hay nhap chieu dai cua hinh chu nhat: ");
             scanf("%f", &a);
-            printf("Xin hay nhap chieu rong cua hinh chu nhat ");
+            printf("Xin hay nhap chieu rong cua hinh chu nhat: ");
             scanf("%f", &b);
             //Tinh chu vi
             c = (a+b)*2;
-            printf("Chu vi cua hinh chu nhat co chieu dai = %f va chieu rong = %f la %.2f\n",a ,b ,c);
+            printf("Chu vi cua hinh chu nhat co chieu dai = %.2f va chieu rong = %.2f la %.2f\n",a ,b ,c);
             //Tinh dien tich
             s = a*b;
-            printf("Dien tich cua hinh chu nhat co chieu dai = %f va chieu rong = %f la %.2f\n",a ,b ,s);
+            printf("Dien tich cua hinh chu nhat co chieu dai = %.2f va chieu rong = %.2f la %.2f\n",a ,b ,s);
+            printf("====================================\n");
+            printf("Ban quyen thuoc ve Tuan Nguyen 62K1 CNTT - Truong Dai hoc Vinh ");
             break;
         }
                 default:
